dafe716301c16ed8472852b48f53f77ea2b72674
AliaksandrChachura <skif198560@gmail.com>
2019-10-15 14:21:05 +0300

    w

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,18 +1,55 @@
 class TicTacToe {
     constructor() {
-
+        this.matrix =  [[null, null, null], [null, null, null], [null, null, null]];
+        this.symbol = 'x';
+        this.rowIndex;
+        this.columnIndex;
     }
 
     getCurrentPlayerSymbol() {
 
+        return this.symbol;
     }
 
-    nextTurn(rowIndex, columnIndex) {
-
+    nextTurn(rowIndex, columnIndex) { 
+        this.rowIndex = rowIndex;
+        this.columnIndex = columnIndex;     
+        if (this.matrix [this.rowIndex][this.columnIndex] === null) {
+            this.matrix [this.rowIndex][this.columnIndex] = this.symbol;
+            if (this.symbol === 'x') {
+                return this.symbol = 'o';
+            }
+            else {
+                return this.symbol = 'x';
+            }
+        }
+        else {
+            return this.symbol;
+        }
     }
 
     isFinished() {
-
+        let row = [];
+        let column = [];
+        let rightDiagonal = [];
+        let leftDiagonal = [];
+        let cI = +this.columnIndex;
+        let rI = +this.rowIndex;
+        for (let i = 0; i < 3; i++) {
+            rightDiagonal.push(this.matrix[i][i]);
+            leftDiagonal.push(this.matrix[3-i-1][i]);
+        }
+        for (let a = 0; a < 3; a++) {
+                column.push(this.matrix[a][cI]);
+                row.push(this.matrix[rI][a]);
+        }
+        if (row.every((num) => num === this.symbol) || column.every((num) => num === this.symbol) || rightDiagonal.every((num) => num === this.symbol) || leftDiagonal.every((num) => num === this.symbol)) {
+            return true;
+        }
+       
+        else {
+            return false;
+        }
     }
 
     getWinner() {
@@ -28,6 +65,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
+        return this.matrix[rowIndex][colIndex];
 
     }
 }
